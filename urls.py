18bc2b9839fa2d3dcea7f42bc49104a9c613bc63
Arnaud Fouchet <foucheta@gmail.com>
2017-03-14 22:31:56 +0100

    Did API to get similars

--- urls.py
+++ urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
     url(r'^logout/$', views.api_logout, name='logout'),
     url(r'^log/$', views.log, name='logs'),
     url(r'^search/$', views.search, name='search'),
+    url(r'^api/similar_articles/(?P<publi_id>[0-9]+)$', views.api_similar_articles,
+        name='api_similar_articles'),
     url(r'^build_advanced_search/$', views.build_advanced_search, name='build_advanced_search'),
     url(r'^advanced_search/$', views.advanced_search, name='advanced_search'),
     url(r'^test/$', views.test, name='test'),
